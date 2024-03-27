91d8951891ca23740bc2df1d8b493432b3cfe5d7
bagusakti <142561975+bagusakti@users.noreply.github.com>
2024-03-27 11:48:39 +0700

    first2

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -24,6 +24,18 @@
             </div>
         </div>
     </div>
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Data Siswa</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Praktik Proyek Kedua</h6>
+          <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci laborum nobis iure nam nostrum. Alias!</p>
+          @if (Auth::check())
+          @else
+          <a href="ss" class="btn btn-primary disabled" aria-disabled="true">Masuk</a>
+          <p class="card-link" style="color: red">silahkan login terlebih dahulu</p>
+          @endif
+        </div>
+      </div>
 </div>
       
 @endsection
